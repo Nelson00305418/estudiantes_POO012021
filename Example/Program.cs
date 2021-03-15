2a935daae1d59fda822b7dc9513f3986d737fc65
Nelson0030518 <00305418@uca.edu.sv>
2021-03-14 19:07:02 -0600

    Add a program that does a sum

---
+++ Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c =0;
+
+            Console.WriteLine("Enter a value:");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another value:");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding the values of {a} and {b} give: {sum(a,b)}");
+
+        }
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
